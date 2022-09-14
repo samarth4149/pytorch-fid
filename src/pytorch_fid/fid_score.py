9443095315f5515b874fc2085b3f1f642a9cd017
Samarth Mishra <samarth4149@users.noreply.github.com>
2022-09-14 14:08:22 -0400

    adding some functions

--- src/pytorch_fid/fid_score.py
+++ src/pytorch_fid/fid_score.py
@@ -86,6 +86,63 @@ class ImagePathDataset(torch.utils.data.Dataset):
             img = self.transforms(img)
         return img
 
+def get_activations_from_dset(
+    dset, model, batch_size=50, dims=2048, device='cpu', num_workers=1):
+    """Calculates the activations of the pool_3 layer for all images.
+
+    Params:
+    -- files       : List of image files paths
+    -- model       : Instance of inception model
+    -- batch_size  : Batch size of images for the model to process at once.
+                     Make sure that the number of samples is a multiple of
+                     the batch size, otherwise some samples are ignored. This
+                     behavior is retained to match the original FID score
+                     implementation.
+    -- dims        : Dimensionality of features returned by Inception
+    -- device      : Device to run calculations
+    -- num_workers : Number of parallel dataloader workers
+
+    Returns:
+    -- A numpy array of dimension (num images, dims) that contains the
+       activations of the given tensor when feeding inception with the
+       query tensor.
+    """
+    model.eval()
+
+    if batch_size > len(dset):
+        print(('Warning: batch size is bigger than the data size. '
+               'Setting batch size to data size'))
+        batch_size = len(dset)
+
+    dataloader = torch.utils.data.DataLoader(dset,
+                                             batch_size=batch_size,
+                                             shuffle=False,
+                                             drop_last=False,
+                                             num_workers=num_workers)
+
+    pred_arr = np.empty((len(files), dims))
+
+    start_idx = 0
+
+    for batch in tqdm(dataloader):
+        imgs = batch[0].to(device)
+
+        with torch.no_grad():
+            pred = model(batch)[0]
+
+        # If model output is not scalar, apply global spatial average pooling.
+        # This happens if you choose a dimensionality not equal 2048.
+        if pred.size(2) != 1 or pred.size(3) != 1:
+            pred = adaptive_avg_pool2d(pred, output_size=(1, 1))
+
+        pred = pred.squeeze(3).squeeze(2).cpu().numpy()
+
+        pred_arr[start_idx:start_idx + pred.shape[0]] = pred
+
+        start_idx = start_idx + pred.shape[0]
+
+    return pred_arr
+
 
 def get_activations(files, model, batch_size=50, dims=2048, device='cpu',
                     num_workers=1):
@@ -116,7 +173,7 @@ def get_activations(files, model, batch_size=50, dims=2048, device='cpu',
         batch_size = len(files)
 
     normalize = TF.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
+                             std=[0.229, 0.224, 0.225])
 
     val_transform = TF.Compose([
         TF.Resize(256, interpolation=3),
@@ -217,7 +274,7 @@ def calculate_activation_statistics(files, model, batch_size=50, dims=2048,
                                     device='cpu', num_workers=1):
     """Calculation of the statistics used by the FID.
     Params:
-    -- files       : List of image files paths
+    -- files       : List of image files paths OR a dataset object
     -- model       : Instance of inception model
     -- batch_size  : The images numpy array is split into batches with
                      batch size batch_size. A reasonable batch size
@@ -232,7 +289,10 @@ def calculate_activation_statistics(files, model, batch_size=50, dims=2048,
     -- sigma : The covariance matrix of the activations of the pool_3 layer of
                the inception model.
     """
-    act = get_activations(files, model, batch_size, dims, device, num_workers)
+    if isinstance(files, torch.utils.data.Dataset):
+        act = get_activations_from_dset(files, model, batch_size, dims, device, num_workers)
+    else:
+        act = get_activations(files, model, batch_size, dims, device, num_workers)
     mu = np.mean(act, axis=0)
     sigma = np.cov(act, rowvar=False)
     return mu, sigma
