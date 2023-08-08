51d9b906d01f49829248691fe758e5233354e92a
Ubuntu <ubuntu@ip-172-31-2-253.us-west-2.compute.internal>
2023-08-08 15:52:02 +0000

    small fix

--- src/pytorch_fid/fid_score.py
+++ src/pytorch_fid/fid_score.py
@@ -136,7 +136,7 @@ def get_activations_from_dset(
             if pred.size(2) != 1 or pred.size(3) != 1:
                 pred = adaptive_avg_pool2d(pred, output_size=(1, 1))
 
-            pred = pred.squeeze(3).squeeze(2).cpu().numpy()
+            pred = pred.squeeze(3).squeeze(2).cpu()
 
         pred_arr[start_idx:start_idx + pred.shape[0]] = pred
 
